7c28fb28fef5a3c3398b91e25a9687eb3f2b1ace
EWU Web.Lab <pmanikoth@ewu.edu>
2024-06-11 00:44:26 +0000

    modify widths

--- src/airtable.js
+++ src/airtable.js
@@ -7,7 +7,7 @@ const AIRTABLE_API_URL = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}`;
 
 export const getRecords = async (tableName) => {
   try {
-    const response = await fetch(`${AIRTABLE_API_URL}/${tableName}`, {
+    const response = await fetch(`${AIRTABLE_API_URL}/${tableName}?sort[0][field]=${'name'}&sort[0][direction]=asc`, {
       headers: {
         Authorization: `Bearer ${AIRTABLE_API_KEY}`,
         'X-Token-ID': AIRTABLE_TOKEN_ID,  // Add Token ID in headers
